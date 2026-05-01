4f3e403bf2f1d1e1cbdfa5e435dd5c07ac8e9f53
Priyanshu <priyanshu@MacBook-Pro-2.local>
2026-05-01 09:26:16 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,61 @@
+// calculator
+
+#include<iostream>
+using namespace std;
+// int main(){
+//     int a;
+//     cout<<"a:";
+//     cin>>a;
+    
+//     int b;
+    
+//     cout<<"b :";
+//     cin>>b;
+//     cout<<"addition o two number:"<<a+b<<endl;
+//     cout<<"substraction o two number:"<<a-b<<endl;
+//     cout<<"division o two number:"<<a/b<<endl;
+//     cout<<"multiplication o two number:"<<a*b<<endl;
+       
+// }
+
+// swap variable
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int c;
+//     int a;
+//     cout<<"a:";
+//     cin>>a;
+//     int b;
+//     cout<<"b:";
+//     cin>>b;
+    // c=b;
+    // b=a;
+    // a=c;
+    // a=a+b;
+    // b=a-b;
+    // a=a-b;
+
+
+    // cout<<a<<" "<<b;
+
+// }
+
+// user se dimension ki length leni h fir uska area niklana h
+#include<iostream>
+using namespace std;
+int main(){     
+    int length;
+    cout<<"length :";
+    cin>>length;
+    int breadth;
+    cout<<"breadth :";
+    cin>>breadth;
+    int radius;
+    cout<<"radius :";
+    cin>>radius;
+    cout<<"area of rectangle :"<<length*breadth<<endl;
+    // area of circle bhi krna h
+    cout<<"area of circle :"<<3.14*radius*radius;
+}
